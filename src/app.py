697b6f98438166684b1ca9609dfc76094a883806
cpizarroi <c.pizarroibaceta@gmail.com>
2024-10-06 00:38:46 -0300

    finalizado

--- src/app.py
+++ src/app.py
@@ -26,17 +26,69 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
+@app.route('/member', methods=['POST'])
+@app.route('/member/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    if request.method == 'GET':
+        if id == None:
+            family = jackson_family.get_all_members()
+            return jsonify(family), 200
+        if id:
+            member = jackson_family.get_member(id)
+            return jsonify(member), 200
 
+    if request.method == 'POST':
+        id = request.json.get("id")
+        first_name = request.json.get("first_name")
+        last_name = "Jackson"
+        age = request.json.get("age")
+        lucky_numbers = request.json.get("lucky_numbers")
 
-    return jsonify(response_body), 200
+        if not first_name:
+            return jsonify({"msg": "first_name is requires"}), 400
+        if not age:
+            return jsonify({"msg": "age is requires"}), 400
+        if not lucky_numbers:
+            return jsonify({"msg": "lucky_numbers is required"}), 400
+        member={
+            "first_name": first_name,
+            "age": age,
+            "lucky_numbers": lucky_numbers,
+            "id": id
+        }
+        jackson_family.add_member(member)
+        return jsonify({"msg":"Member created successfully"}), 200
+
+    if request.method =="PUT":
+        member = jackson_family.get_member(id)    
+        first_name = request.json.get("first_name")
+        last_name = "Jackson"
+        age = request.json.get("age")
+        lucky_numbers = request.json.get("lucky_numbers")
+
+        if not first_name:
+               return jsonify({"msg": "first_name is requires"}), 400
+        if not age:
+            return jsonify({"msg": "age is requires"}), 400
+        if not lucky_numbers:
+            return jsonify({"msg": "lucky_numbers is required"}), 400
+
+        member["first_name"] = first_name
+        member["age"] = age
+        member["lucky_numbers"] = lucky_numbers
+
+        return jsonify({"msg": "Member created successfully"}, member), 200
+
+    if request.method =="DELETE":
+        member = jackson_family.get_member(id)
+        if not member:
+            return jsonify({"msg": "Member not found"}), 404
+        else:
+            jackson_family.delete_member(id)
+            return jsonify({"done": True}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/test_solution.py
+++ src/test_solution.py
@@ -1,113 +1,61 @@
-import pytest, os, sys, tempfile, mock, json
-from flask import Flask
 
-@pytest.fixture
-def client():
-    with mock.patch('flask.Flask', lambda x: Flask(x)):
-        from app import app
-        db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-        app.config['TESTING'] = True
+"""
+update this file to implement the following already declared methods:
+- add_member: Should add a member to the self._members list
+- delete_member: Should delete a member from the self._members list
+- update_member: Should update a member from the self._members list
+- get_member: Should return a member from the self._members list
+"""
+from random import randint
 
-        with app.test_client() as client:
-            # with app.app_context():
-            #     app.init_db()
-            yield client
+class FamilyStructure:
+    def __init__(self, last_name):
+        self.last_name = last_name
 
-        os.close(db_fd)
-        os.unlink(app.config['DATABASE'])
-	
-@pytest.mark.it("The Family structure has to be initialized with the 3 members specified in the instructions")
-def test_first_three(client):
-    response = client.get('/members')
-    members = json.loads(response.data)
-    assert len(members) == 3
+        # example list of members
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+        },
+         {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        },
+        {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [1]
 
-@pytest.mark.it("Implement method POST /member to add a new member")
-def test_add_implementation(client):
-    response = client.post('/member', json={
-		"first_name": "Tommy",
-        "id": 3443,
-		"age": 23,
-		"lucky_numbers": [34,65,23,4,6]
-	})
-    assert response.status_code == 200
+        }]
 
-@pytest.mark.it("Method POST /member should return something, NOT EMPTY")
-def test_add_empty_reponse_body(client):
-    response = client.post('/member', json={
-		"first_name": "Sandra",
-		"age": 12,
-        "id": 4446,
-		"lucky_numbers": [12,34,33,45,32,12]
-	})
-    assert response.data != b""
+    # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return randint(0, 99999999)
 
-@pytest.mark.it("Implement method GET /members")
-def test_get_members_exist(client):
-    response = client.get('/members')
-    assert response.status_code == 200
+    def add_member(self, member):
+        if member['id'] is None: member["id"]=self._generateId() 
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return member
 
-@pytest.mark.it("Method GET /members should return a list")
-def test_get_members_returns_list(client):
-    response = client.get('/members')
-    data = json.loads(response.data)
-    assert isinstance(data, list)
+    def delete_member(self, id):
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == id:
+                self._members.pop(position)
+                return self._members
 
-@pytest.mark.it("We added two members using POST /member, when calling the GET /members should get a list of length == 5")
-def test_get_members_returns_list_of_five(client):
-    response = client.get('/members')
-    members = json.loads(response.data)
-    assert len(members) == 5
+    def get_member(self, id):
+        miembro = list(filter(lambda member: member["id"] == id,self._members))
+        return miembro[0]
 
-@pytest.mark.it("Method GET /member/<int:id> should exist")
-def test_get_single_member_implemented(client):
-    response = client.get('/member/3443')
-    assert response.status_code == 200
-
-@pytest.mark.it("Method GET /member/<int:id> should return a one single family member in a dictionary format")
-def test_get_single_member_returns_dict(client):
-    response = client.get('/member/3443')
-    data = json.loads(response.data)
-    assert data is not None
-    assert isinstance(data, dict)
-
-@pytest.mark.it("The dictionary returned by GET /member/<int:id> should contain one family member with the keys [name, id, age, lucky_numbers]")
-def test_get_single_member_has_keys(client):
-    response = client.get('/member/3443')
-    data = json.loads(response.data)
-
-    assert data is not None
-    assert "first_name" in data
-    assert "id" in data
-    assert "age" in data
-    assert "lucky_numbers" in data
-
-@pytest.mark.it("Method GET /member/3443 should return Tommy")
-def test_get_first_member_tommy(client):
-    response = client.get('/member/3443')
-    data = json.loads(response.data)
-    assert data is not None
-    assert "first_name" in data
-    assert data["first_name"] == "Tommy"
-
-@pytest.mark.it("Implement method DELETE /member/<int:id> to delete a family member")
-def test_delete_member(client):
-    response = client.delete('/member/3443')
-    assert response.status_code == 200
-
-@pytest.mark.it("Method DELETE /member/3443 should return dictionary with 'done' key")
-def test_delete_response(client):
-    client.post('/member', json={
-		"first_name": "Tommy",
-        "id": 3443,
-		"age": 23,
-		"lucky_numbers": [34,65,23,4,6]
-	})
-    response = client.delete('/member/3443')
-    assert response.json["done"] == True
-
-@pytest.mark.it("After deleting the member 3443 we called GET /members and it should return a list with 4 members")
-def test_get_members_returns_list_of_four(client):
-    response = client.get('/members')
-    members = json.loads(response.data)
-    assert len(members) == 4
+    # this method is done, it returns a list with all the family members
+    def get_all_members(self):
+        return self._members
